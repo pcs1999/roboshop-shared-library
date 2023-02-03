0f39bc89eea0dd313cd2dfb2e96a00e1ffd47d3e
chandra <chandra@gmail.com>
2023-02-03 11:41:13 +0530

    variable declaration

---
+++ vars/test1.groovy
@@ -0,0 +1,16 @@
+pipeline {
+    agent any 
+    stages {
+        stage('test') {
+         steps {
+            script {
+                def chandra = "first name"
+                def shekar = "last name"
+
+                print chandra
+                print shekar 
+            }
+         }
+        }
+    }
+}

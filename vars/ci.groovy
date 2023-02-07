e73e7e2f86c413216d59c6d353d06c296c22f12c
chandra <chandra@gmail.com>
2023-02-07 19:01:59 +0530

    aws parameter true npm missing

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -31,7 +31,7 @@ def call() {
           }
           steps {
 
-            sh "sonar-scanner -Dsonar.host.url=http://172.31.12.42:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+            sh "sonar-scanner -Dsonar.host.url=http://172.31.12.42:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component}"
           }
         }
 

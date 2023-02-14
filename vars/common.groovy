44c38d7827a0285e2416321c71c1a503805c73f0
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-02-14 15:22:32 +0530

    zip file

--- vars/common.groovy
+++ vars/common.groovy
@@ -34,7 +34,14 @@ def email(email_note) {
 def artifactPush() {
   sh "echo ${TAG_NAME} >VERSION"
   if (app_lang == "nodejs") {
-    sh "zip -r cart-${TAG_NAME}.zip node_modules server.js VERSION"
+    sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION"
   }
   sh 'ls -l'
+
+  NEXUS_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+        NEXUS_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+        wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${NEXUS_PASS}", var: 'SECRET']]]) {
+            sh "curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.6.153:8081/repository/maven-releases/${component}/${component}-${TAG_NAME}.zip"
+        }
+
 }

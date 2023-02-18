f1b8bb82041cac8e98570ab7e84ca03c14c23f42
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-02-18 15:38:58 +0530

    tf_change2

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -2,7 +2,7 @@ def call () {
     pipeline {
         agent {
             node {
-                label = 'work-station'
+                label 'work-station'
             }
         
             

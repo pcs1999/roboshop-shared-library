4e3d7ad8c83d15dab45abde19bd6371bbbdec73f
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-04-13 11:46:03 +0530

    updating ./roboshop-shared-library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,59 +1,55 @@
 def call() {
 
-  if(!env.java_opt) {
-    env.java_opt = " "
-  }
+  pipeline{
 
-  if(!env.extrafiles) {
-    env.extrafiles = " "
-  }
- 
- if (!env.TAG_NAME){
-     env.PUSH_CODE = 'false' 
- } else {
-  env.PUSH_CODE = 'true'
- }
+    agent {
 
-  try {
-    node('work-station') {
-
-      stage('Checkout') {
-        cleanWs()
-        git branch: 'main', url: "https://github.com/pcs1999/${component}.git"
-        sh 'env'
-      }
-
-      stage('Compile/Build') {
-        common.compile()
-      }
-
-      stage('Unit Tests') {
-        common.unittests()
-        
-      }
-
-      stage('Quality Control') {
-        SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-        SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-        wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
-          sh "sonar-scanner -Dsonar.host.url=http://172.31.12.42:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${java_opt}"
-        }
-      }
-      if (app_lang == "maven") {
-        stage ('Build package') {
-          sh "mvn package && cp target/${component}-1.0.jar ${component}.jar"
-        }
-      }
-      if (env.PUSH_CODE == 'true') {
-      stage('Upload Code to Centralized Place') {
-        common.artifactPush()
-      } 
-      }
-      
+      label 'workstation'
 
     }
 
-  } catch(Exception e) {
-    common.email("Failed")
+    stages {
+
+      stage ('compile/build') {
+
+        steps {
+
+          echo 'compile'
+        }
+      }
+
+
+      stage ('unit/test') {
+
+        steps {
+
+          echo 'unit-test'
+        }
+      }
+
+      stage ('quality controll') {
+
+        steps {
+
+          echo 'quality controll'
+        }
+      }
+
+    }
+
+
+    stage ('upload code to centralized place') {
+
+      steps {
+
+        echo 'upload code to centralized place'
+      }
+    }
+
   }
+
+
+
+
 }
+
--- vars/test1.groovy
+++
@@ -1,9 +0,0 @@
-def new1() {
-    print "hellow"
-    def abc= "hyderabad"
-
-    print abc
-if ( pcs== "super") {
-    print "satisfied"
-} else print {"not satisfied"}
-}

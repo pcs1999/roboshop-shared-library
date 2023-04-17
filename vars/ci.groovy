7b8a406205c20f05fe1a8f896081fbd6b6595552
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-04-17 12:59:07 +0530

    updating dns jenkins record

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,21 +1,22 @@
 def call() {
 
-  if(!env.sonar_extra_opt){
-    env.sonar_extra_opt=" "
+  if(!env.SONAR_EXTRA_OPTS) {
+    env.SONAR_EXTRA_OPTS = " "
   }
-  if(!env.TAG_NAME){
-    env.PUSH_CODE="false"}
-    else {
-      env.PUSH_CODE="true"
-    }
+
+  if(!env.TAG_NAME) {
+    env.PUSH_CODE = "false"
+  } else {
+    env.PUSH_CODE = "true"
   }
+
   try {
     node('workstation') {
 
-      stage('scripted checkout of scm') {
+      stage('Checkout') {
         cleanWs()
-        git branch: 'main', url: "https://github.com/pcs1999/${component}.git"
-        sh "env"
+        git branch: 'main', url: "https://github.com/raghudevopsb70/${component}"
+        sh 'env'
       }
 
       stage('Compile/Build') {
@@ -30,24 +31,21 @@ def call() {
         SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
         SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
         wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
-          sh "sonar-scanner -Dsonar.host.url=http://sonarqube.chandupcs.online:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${sonar_extra_opt}"
-         //sh "echo sonar scan"
+          sh "sonar-scanner -Dsonar.host.url=http://sonarqube.chandupcs.online:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
+          //sh "echo Sonar Scan"
         }
-
-
       }
-      if(env.PUSH_CODE=="true") {
+
+      if(env.PUSH_CODE == "true") {
         stage('Upload Code to Centralized Place') {
           echo 'Upload'
         }
+      }
 
 
+    }
 
-
-
-    } }
-
-  }
-  catch(Exception e) {
+  } catch(Exception e) {
     common.email("Failed")
   }
+}

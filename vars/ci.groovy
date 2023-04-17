1d97e34169522162a753ccc8fa717963be147f25
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-04-17 10:22:10 +0530

    updating dns jenkins record

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -24,7 +24,10 @@ def call() {
         SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
         wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
           sh "sonar-scanner -Dsonar.host.url=http://sonarqube.chandupcs.online:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${sonar_extra_opt}"
+         //sh "echo sonar scan"
         }
+
+
       }
 
       stage('Upload Code to Centralized Place') {

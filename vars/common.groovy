e7b755d966a98d5fa34e2cc17478b9e742ff9e88
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-04-18 14:24:53 +0530

    code

--- vars/common.groovy
+++ vars/common.groovy
@@ -35,8 +35,7 @@ def artifactpush (){
     NEXUS_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
     NEXUS_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
     wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${NEXUS_PASS}", var: 'SECRET']]]) {
-        sh "curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip  http://172.31.15.218:8081/repository/${component}/${component}-${TAG_NAME}.zip"
-    }
+        sh "curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.15.218:8081/repository/${component}/${component}-${TAG_NAME}.zip"}
     }
 
 def email (email_note){

f17fc463a6e77d802514817ac956d7d3bbc58ccf
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-04-18 18:04:54 +0530

    changing downloading url from amazon to nexus

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,11 +1,11 @@
 def compile () {
     if (app_lang == "nodejs") {
         sh 'npm install'
-        sh 'env'
+
     }
 
     if (app_lang == "maven") {
-        sh "mvn package && cp target/${component}-1.0.jar ${component}.jar"
+        sh "mvn clean compile"
     }
 }
 
@@ -25,8 +25,15 @@ def unittests () {
 
 }
 
+def email (email_note){
+    mail bcc: '', body: "failure in : ${JOB_BASE_NAME} pipeline\nTake a look with url\nDisplay_URL:${RUN_DISPLAY_URL}\n jenkins_URL:${JENKINS_URL}" , cc: '', from: 'pcs04031999@gmail.com', replyTo: '', subject: "Jenkins job:${JOB_BASE_NAME} Failure notification", to: 'cp7524420@gmail.com'
+}
+
 def artifactpush (){
     sh "echo ${TAG_NAME} >VERSION"
+
+    sh 'env'
+
     if (app_lang == "nodejs") {
         sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION ${extra_files}"
     }
@@ -44,9 +51,7 @@ def artifactpush (){
     NEXUS_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
     NEXUS_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
     wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${NEXUS_PASS}", var: 'SECRET']]]) {
-        sh "curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.15.218:8081/repository/${component}/${component}-${TAG_NAME}.zip"}
+        sh "curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.15.218:8081/repository/${component}/${component}-${TAG_NAME}.zip"
     }
-
-def email (email_note){
-    mail bcc: '', body: "failure in : ${JOB_BASE_NAME} pipeline\nTake a look with url\nDisplay_URL:${RUN_DISPLAY_URL}\n jenkins_URL:${JENKINS_URL}" , cc: '', from: 'pcs04031999@gmail.com', replyTo: '', subject: "Jenkins job:${JOB_BASE_NAME} Failure notification", to: 'cp7524420@gmail.com'
 }
+

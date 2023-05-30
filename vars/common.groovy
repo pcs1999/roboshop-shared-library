cf372b934855abc70a9b0609d0f2f39d42f0964c
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-05-30 19:05:35 +0530

    iamges_creation

--- vars/common.groovy
+++ vars/common.groovy
@@ -7,6 +7,8 @@ def compile () {
     if (app_lang == "maven") {
         sh "mvn clean compile"
     }
+
+    sh "docker build -t 261454514620.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} . "
 }
 
 def unittests () {
@@ -30,28 +32,33 @@ def email (email_note){
 }
 
 def artifactpush (){
-    sh "echo ${TAG_NAME} >VERSION"
 
-    sh 'env'
+    sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 261454514620.dkr.ecr.us-east-1.amazonaws.com"
 
-    if (app_lang == "nodejs") {
-        sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION ${extra_files}"
-    }
+    sh "docker push 261454514620.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}"
 
-
-    if (app_lang == "Nginx" || app_lang == "python") {
-        sh "zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile ${extra_files}"
-    }
-
-    if (app_lang == "maven") {
-        sh "zip -r ${component}-${TAG_NAME}.zip  ${component}.jar VERSION ${extra_files}"
-    }
-
-
-    NEXUS_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-    NEXUS_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-    wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${NEXUS_PASS}", var: 'SECRET']]]) {
-        sh "curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.92.158:8081/repository/${component}/${component}-${TAG_NAME}.zip"
-    }
+//    sh "echo ${TAG_NAME} >VERSION"
+//
+//    sh 'env'
+//
+//    if (app_lang == "nodejs") {
+//        sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION ${extra_files}"
+//    }
+//
+//
+//    if (app_lang == "Nginx" || app_lang == "python") {
+//        sh "zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile ${extra_files}"
+//    }
+//
+//    if (app_lang == "maven") {
+//        sh "zip -r ${component}-${TAG_NAME}.zip  ${component}.jar VERSION ${extra_files}"
+//    }
+//
+//
+//    NEXUS_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+//    NEXUS_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+//    wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${NEXUS_PASS}", var: 'SECRET']]]) {
+//        sh "curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.92.158:8081/repository/${component}/${component}-${TAG_NAME}.zip"
+//    }
 }
 
